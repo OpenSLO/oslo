7913f958b5c257cccc9cc201efed5a17e7381bb6
ian-bartholomew <ibartholomew@gmail.com>
2021-05-02 23:41:47 -0700

    Description update

--- cmd/root.go
+++ cmd/root.go
@@ -30,13 +30,13 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "oslo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Oslo is a CLI tool for the OpenSLO spec",
+	//   Long: `A longer description that spans multiple lines and likely contains
+	// examples and usage of using your application. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	// Cobra is a CLI library for Go that empowers applications.
+	// This application is a tool to generate the needed files
+	// to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
