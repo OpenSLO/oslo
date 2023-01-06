f95a710e10a763870ac5b6a3f0f9a6d38e9a223c
ian-bartholomew <ian@nobl9.com>
2023-01-06 15:43:47 -0800

    More debugging

--- test/cli/install-deps.sh
+++ test/cli/install-deps.sh
@@ -6,7 +6,6 @@ LIBS_BATS_ASSERT_VERSION="2.1.0"
 echo -n "Installing bats dependencies"
 # change our working dir to the test folder that this lives in
 cd "$(dirname $0)" || exit
-pwd
 mkdir -p ./test_helper/bats-support
 echo -n "."
 curl -sSL https://github.com/ztombol/bats-support/archive/refs/tags/v${LIBS_BATS_SUPPORT_VERSION}.tar.gz -o /tmp/bats-support.tgz
@@ -26,3 +25,5 @@ rm -rf /tmp/bats-assert.tgz
 echo -n "."
 
 echo "Done!"
+
+ls -al
