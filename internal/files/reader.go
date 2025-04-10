7562bb0b65db59364a46b56a350ea4351ac705b0
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2025-04-10 22:12:48 +0200

    initial tests and remove license notices

--- internal/files/reader.go
+++ internal/files/reader.go
@@ -1,24 +1,8 @@
-/*
-Package yamlutils provides functions to parse OpenSLO manifests.
-
-# Copyright © 2022 OpenSLO Team
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package files
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -31,20 +15,25 @@ import (
 )
 
 // ReadObjects reads [openslo.Object] from the provided sources.
-func ReadObjects(sources []string) ([]openslo.Object, error) {
-	var allObjects []openslo.Object
+// It returns a map where the key is a file path and the value are objects read form this file.
+func ReadObjects(sources []string) (map[string][]openslo.Object, error) {
+	allObjects := make(map[string][]openslo.Object)
 	for _, src := range sources {
-		data, err := readRawSchema(src)
+		objects, err := readObjectsFromSource(src)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to read objects from %s: %w", src, err)
 		}
-		objects, err := readObjectsFromRawData(data)
-		if err != nil {
-			return nil, err
-		}
-		allObjects = append(allObjects, objects...)
+		allObjects[src] = objects
 	}
 	return allObjects, nil
+}
+
+func readObjectsFromSource(source string) ([]openslo.Object, error) {
+	data, err := readRawSchema(source)
+	if err != nil {
+		return nil, err
+	}
+	return readObjectsFromRawData(data)
 }
 
 // readObjectsFromRawData reads [openslo.Object] from a byte slice.
--- test/validate.bats
+++ test/validate.bats
@@ -8,7 +8,19 @@ setup() {
 }
 
 @test "validate an invalid api version" {
-  run oslo fmt -f "${TEST_SUITE_INPUTS}/invalid-apiversion.yaml"
+  run oslo validate -f "${TEST_SUITE_INPUTS}/invalid-apiversion.yaml"
+  assert_failure
+  assert_output "Error: failed to read objects from /oslo/test/inputs/invalid-apiversion.yaml: error unmarshaling JSON: while decoding JSON: failed to decode object: unsupported openslo.Version: foo"
+}
+
+@test "validate unknown field" {
+  run oslo validate -f "${TEST_SUITE_INPUTS}/unknown-field.yaml"
+  assert_failure
+  assert_output "Error: failed to read objects from ${TEST_SUITE_INPUTS}/unknown-field.yaml: failed to decode openslo/v1 Service: json: unknown field \"markdown\""
+}
+
+@test "v1alpha" {
+  run oslo validate -f "${TEST_SUITE_INPUTS}/v1alpha"
   assert_failure
   assert_output "Error: issue parsing objects: error unmarshaling JSON: while decoding JSON: failed to decode object: unsupported openslo.Version: foo"
 }
