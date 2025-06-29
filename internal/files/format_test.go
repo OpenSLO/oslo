7d6c98ed4e9f6124ee21eab5f39212686ee53cfb
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2025-06-29 13:00:07 +0200

    adjust unit tests

--- internal/files/format_test.go
+++ internal/files/format_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/OpenSLO/oslo/internal/files"
-	"github.com/OpenSLO/oslo/internal/pathutils"
 )
 
 func TestFormatFiles(t *testing.T) {
@@ -22,17 +21,16 @@ func TestFormatFiles(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "Invalid file",
+			name:    "invalid file",
 			files:   []string{"v0alpha/invalid-file.yaml"},
 			format:  openslosdk.FormatYAML,
 			wantErr: true,
 			wantOut: "",
 		},
 		{
-			name:    "Invalid content",
-			files:   []string{"v1alpha/invalid-service.yaml"},
-			format:  openslosdk.FormatYAML,
-			wantErr: false,
+			name:   "invalid content",
+			files:  []string{"invalid-service.yaml"},
+			format: openslosdk.FormatYAML,
 			wantOut: `- apiVersion: openslo/v1alpha
   kind: Service
   metadata:
@@ -40,18 +38,12 @@ func TestFormatFiles(t *testing.T) {
     name: my-rad service
   spec:
     description: This is a great description of an even better service.
-- apiVersion: openslo/v1alpha
-  kind: Service
-  metadata:
-    name: this
-  spec: {}
 `,
 		},
 		{
-			name:    "Passes single file",
-			files:   []string{"v1alpha/valid-service.yaml"},
-			format:  openslosdk.FormatYAML,
-			wantErr: false,
+			name:   "formats single YAML file",
+			files:  []string{"valid-service.yaml"},
+			format: openslosdk.FormatYAML,
 			wantOut: `- apiVersion: openslo/v1alpha
   kind: Service
   metadata:
@@ -62,10 +54,9 @@ func TestFormatFiles(t *testing.T) {
 `,
 		},
 		{
-			name:    "Passes single JSON file",
-			files:   []string{"v1alpha/valid-service.yaml"},
-			format:  openslosdk.FormatJSON,
-			wantErr: false,
+			name:   "formats single JSON file",
+			files:  []string{"valid-service.yaml"},
+			format: openslosdk.FormatJSON,
 			wantOut: `[
   {
     "apiVersion": "openslo/v1alpha",
@@ -82,10 +73,9 @@ func TestFormatFiles(t *testing.T) {
 `,
 		},
 		{
-			name:    "Passes multiple files",
-			files:   []string{"v1alpha/valid-service.yaml", "v1alpha/valid-service.yaml"},
-			format:  openslosdk.FormatYAML,
-			wantErr: false,
+			name:   "formats multiple YAML files",
+			files:  []string{"valid-service.yaml", "valid-service.yaml"},
+			format: openslosdk.FormatYAML,
 			wantOut: `- apiVersion: openslo/v1alpha
   kind: Service
   metadata:
@@ -103,16 +93,99 @@ func TestFormatFiles(t *testing.T) {
     description: This is a great description of an even better service.
 `,
 		},
+		{
+			name:   "formats a list of services",
+			files:  []string{"list-of-services.yaml"},
+			format: openslosdk.FormatYAML,
+			wantOut: `- apiVersion: openslo/v1
+  kind: Service
+  metadata:
+    name: my-service-1
+  spec: {}
+- apiVersion: openslo/v1
+  kind: Service
+  metadata:
+    name: my-service-2
+  spec: {}
+`,
+		},
+		{
+			name:   "formats two documents",
+			files:  []string{"two-documents.yaml"},
+			format: openslosdk.FormatYAML,
+			wantOut: `- apiVersion: openslo/v1
+  kind: Service
+  metadata:
+    name: my-service-1
+  spec: {}
+- apiVersion: openslo/v1
+  kind: Service
+  metadata:
+    name: my-service-2
+  spec: {}
+- apiVersion: openslo/v1
+  kind: Service
+  metadata:
+    name: my-service-3
+  spec: {}
+`,
+		},
+		{
+			name:   "formats two documents in JSON",
+			files:  []string{"two-documents.yaml"},
+			format: openslosdk.FormatJSON,
+			wantOut: `[
+  {
+    "apiVersion": "openslo/v1",
+    "kind": "Service",
+    "metadata": {
+      "name": "my-service-1"
+    },
+    "spec": {}
+  },
+  {
+    "apiVersion": "openslo/v1",
+    "kind": "Service",
+    "metadata": {
+      "name": "my-service-2"
+    },
+    "spec": {}
+  },
+  {
+    "apiVersion": "openslo/v1",
+    "kind": "Service",
+    "metadata": {
+      "name": "my-service-3"
+    },
+    "spec": {}
+  }
+]
+`,
+		},
+		{
+			name:   "formats single JSON doc to YAML",
+			files:  []string{"valid-service.json"},
+			format: openslosdk.FormatYAML,
+			wantOut: `- apiVersion: openslo/v1alpha
+  kind: Service
+  metadata:
+    displayName: My Rad Service
+    name: my-rad-service
+  spec:
+    description: This is a great description of an even better service.
+`,
+		},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			out := &bytes.Buffer{}
 			for i, file := range tc.files {
-				tc.files[i] = filepath.Join(pathutils.FindModuleRoot(), "test", "inputs", file)
+				tc.files[i] = filepath.Join("testdata", "format", file)
 			}
-			if err := files.Format(out, tc.format, tc.files); (err != nil) != tc.wantErr {
-				t.Errorf("fmtFile() error = %v, wantErr %v", err, tc.wantErr)
+			err := files.Format(out, tc.format, tc.files)
+			if tc.wantErr {
+				assert.Error(t, err)
 				return
 			}
 			assert.Equal(t, tc.wantOut, out.String())
